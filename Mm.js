2cba224fcce51760f7b632b03366b543cf35c1a2
aremumahmud <74737123+aremumahmud@users.noreply.github.com>
2022-08-10 15:05:43 +0100

    Add files via upload

---
+++ Mm.js
@@ -0,0 +1,21 @@
+var superagent = require("superagent")
+var count= 0
+var errr = 0
+function respawn(url){
+if(count == 20000 || errr==20000){
+    console.log("suc" , count)
+  console.log("err" , errr)
+  }
+  superagent
+     .get(url)
+     .end((err , res)=>{
+       if(err){
+         errr++
+       }else{
+         count++
+       }
+       respawn(url)
+       
+     })
+}
+respawn("http://alpidoveon.com/4/3587551")
